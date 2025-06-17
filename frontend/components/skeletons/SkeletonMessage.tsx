d0629dcf921be86add5e44e54aea873aa26e1187
Devesh Chandra <deveshru2712@gmail.com>
2025-06-17 15:42:05 +0530

    very small issue fix

--- frontend/components/skeletons/SkeletonMessage.tsx
+++ frontend/components/skeletons/SkeletonMessage.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import MessageNav from "../MessageNav";
 
 export default function SkeletonMessage() {
+  const dummyUser: User = {
+    id: "1",
+    email: "example.com",
+    username: "user",
+    conversations: [],
+  };
   return (
     <div className="h-full flex flex-col mx-6 py-4">
       <div className="flex-1 flex-col flex gap-1"></div>
       <div className="w-full">
-        <MessageNav />
+        <MessageNav user={dummyUser} />
       </div>
     </div>
   );
